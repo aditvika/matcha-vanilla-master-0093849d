bb2766a2adfce28d35294be75d945885ba2e686b
Adityo Saputra <tyozxtar@gmail.com>
2026-09-07 19:29:44 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
+  base: './', //
   vite: {
     build: {
       rollupOptions: {
