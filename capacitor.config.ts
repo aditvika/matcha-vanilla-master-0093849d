cbdff75235d2868ac6a7d1d226383832adb182f1
Adityo Saputra <tyozxtar@gmail.com>
2026-09-07 14:38:29 +0800

    a

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "app.lovable.matcha_vanilla_master.twa",
   appName: "MVMaster X",
-  webDir: "dist/client",
+  webDir: ".output/public",
   android: {
     allowMixedContent: false,
   },
